626bc3b2ee7c6c8e488edf423540872ed21784e7
Sergey Vishnevsky <serejich@live.com>
2021-06-27 14:35:01 +0300

    Add tests for TicketDetailsItem component

---
+++ src/__tests__/components/TicketDetailsItem.spec.tsx
@@ -0,0 +1,24 @@
+import { screen, render } from '@testing-library/react';
+import TicketDetailsItem from '@/components/TicketDetailsItem';
+
+describe('Tab', () => {
+  it('should render title correctly', () => {
+    render(<TicketDetailsItem title='0 пересадок' text='' />);
+
+    const ticketDetailsItemTitle = screen.getByTestId(
+      'ticket-details-item-title'
+    );
+
+    expect(ticketDetailsItemTitle).toHaveTextContent('0 пересадок');
+  });
+
+  it('should render text correctly', () => {
+    render(<TicketDetailsItem title='0 пересадок' text='' />);
+
+    const ticketDetailsItemText = screen.getByTestId(
+      'ticket-details-item-text'
+    );
+
+    expect(ticketDetailsItemText).toHaveTextContent('');
+  });
+});
